41f47b5febbf9b2260e75bbf74878e5f69ea9e6f
Camilo <camilo@dt360group.com>
2023-05-30 11:03:15 -0400

    ADD: Añadido mensajes cuando se intenta hacer una petición get o post al index

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  console.log('get index')
   res.render('index', { title: 'Express' });
 });
+
+router.post('/', function(req, res, next) {
+  console.log("POST index")
+  console.log(req.body)
+})
 
 module.exports = router;
