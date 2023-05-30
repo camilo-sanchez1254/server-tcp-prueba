e78716b7679ada94d95f8ffb1d75aa1092712f0e
Camilo <camilo@dt360group.com>
2023-05-30 11:10:18 -0400

    FIX: Ya no envia las vistas

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,13 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   console.log('get index')
-  res.render('index', { title: 'Express' });
+  res.send('GET index');
 });
 
 router.post('/', function(req, res, next) {
   console.log("POST index")
   console.log(req.body)
+  res.send("POST index")
 })
 
 module.exports = router;
